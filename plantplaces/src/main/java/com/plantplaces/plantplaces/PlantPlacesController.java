30cdcaf623b7b94039a985a29fa10ee57d5b2866
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 14:54:28 -0400

    Update / endpoint

--- plantplaces/src/main/java/com/plantplaces/plantplaces/PlantPlacesController.java
+++ plantplaces/src/main/java/com/plantplaces/plantplaces/PlantPlacesController.java
@@ -3,16 +3,25 @@ package com.plantplaces.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Administrator
+ * Handle the /start endpoint
+ * @return
+ */
 @Controller
 public class PlantPlacesController {
-	/**
-	 * Handle is starting endpoint
-	 */
+
 	@RequestMapping("/start")
 
 	public String start() {
 		return "start";
 	}
+	
+	
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */	
 	@RequestMapping("/")
 
 	public String index() {
